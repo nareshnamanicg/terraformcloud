1874903c02adecebe1ecf17a172b2ca2b129aba3
nareshnamanicg <85550135+nareshnamanicg@users.noreply.github.com>
2021-07-28 20:26:45 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,15 +1,15 @@
 # Authentication
 provider "azurerm" {
-  features {}
-  subscription_id = var.tf_var_arm_subscription_id
-  client_id       = var.tf_var_arm_client_id
-  client_secret   = var.tf_var_arm_client_secret
-  tenant_id       = var.tf_var_arm_tenant_id
+	features {}
+	subscription_id = var.tf_var_arm_subscription_id
+	client_id  		= var.tf_var_arm_client_id
+	client_secret 	= var.tf_var_arm_client_secret
+	tenant_id 		= var.tf_var_arm_tenant_id
 }
 
 # Variable declaration
 variable "tf_var_arm_subscription_id" {
-  description = "enter subscription id"
+	description = "enter subscription id"
 }
 
 variable "tf_var_arm_client_id" {
@@ -24,39 +24,37 @@ variable "tf_var_arm_tenant_id" {
   description = "Enter tenant ID"
 }
 
-variable "resourceGroupName" {
-  type        = string
-  default     = "demo-rg"
-  description = "Resource Group for this deployment."
+variable "resource_group_name" {
+	type = string
+	default = "terraform-rg"
+	description = "Enter the resource group name"
+}
+
+variable "virtual_machine_size" {
+	type = string
+	default = "Standard_DS1_v2"
+	description = "Virtual machine size (has to be at least the size of Standard_A3 to support 2 NICs)"
+}
+
+variable "admin_username" {
+	type = string
+	default = "demo_user"
+	description= "Default Admin username"
+}
+
+variable "admin_password" {
+	type = string
+	description = "Default Admin password"
+}
+
+variable "storage_account_type" {
+	type = string
+	default = "Standard"
+	description = "Storage Account type for the VM and VM diagnostic storage"
 }
 
 variable "location" {
-  type        = string
-  default     = "westus"
-  description = "Enter the location for all resources."
-}
-
-variable "adminUsername" {
-  type        = string
-  description = "User name for the Virtual Machine"
-}
-
-variable "vmName" {
-  type        = string
-  description = "Name for the Virtual Machine"
-}
-
-variable "adminPasswordOrKey" {
-  type        = string
-  description = "SSH Key or password for the Virtual Machine. SSH key is recommended."
-}
-
-variable "authenticationType" {
-  type        = string
-  default     = "sshPublicKey"
-  description = "Type of authentication to be used on the Virtual Machine. SSH key is recommended but if you want to use password authentication use 'password'"
-  validation {
-    condition     = var.authenticationType == "sshPublicKey" || var.authenticationType == "password"
-    error_message = "Authentication type hould be either \"ssh\" or \"password\"."
-  }
+	type = string
+	default = "westus"
+	description = "Enter the location for all resources."
 }
