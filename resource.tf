79af93986720599000aa7c8f7f88b4ba434aa4bf
nareshnamanicg <85550135+nareshnamanicg@users.noreply.github.com>
2021-07-28 14:30:47 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -1,3 +1,15 @@
+# Configure the Microsoft Azure Provider
+terraform {
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
 
 # Create a resource group if it doesn't exist
 resource "azurerm_resource_group" "myterraformgroup" {
