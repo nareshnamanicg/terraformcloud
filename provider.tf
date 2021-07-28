46b10099c696b0c249f66b1f7661712119b9a65a
nareshnamanicg <85550135+nareshnamanicg@users.noreply.github.com>
2021-07-28 14:25:36 +0530

    Updated files

---
+++ provider.tf
@@ -0,0 +1,19 @@
+# Azure Provider source and version being used
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
+}
+
+# Configure the Microsoft Azure Provider
+provider "azurerm" {
+  features {}
+
+  subscription_id = "6b806999-887d-4690-9e0b-dcb8b0504009"
+  client_id       = "9e47a648-bc41-4c9e-b218-8f11d46b06ac"
+  client_secret   = 777567a3-d96a-48a2-8e56-9cf9005790be
+  tenant_id       = "46e62401-e0cb-44e7-b9bf-90bf637a3628"
+}
