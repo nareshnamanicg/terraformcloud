921dc4f8afba6753de1413027550834bd3a80435
nareshnamanicg <85550135+nareshnamanicg@users.noreply.github.com>
2021-07-28 15:35:47 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -7,16 +7,11 @@ terraform {
     }
   }
 }
-provider "azure" {
+provider "azurerm" {
   features {}
   # Configuration options
   subscription_id = "6b806999-887d-4690-9e0b-dcb8b0504009"
   client_id       = "9e47a648-bc41-4c9e-b218-8f11d46b06ac"
   client_secret   = "777567a3-d96a-48a2-8e56-9cf9005790be"
   tenant_id       = "46e62401-e0cb-44e7-b9bf-90bf637a3628"
-}
-
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
-  location = "West Europe"
 }
