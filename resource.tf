eaa783d7b530820b4f4f435e94e237a06de8de8f
nareshnamanicg <85550135+nareshnamanicg@users.noreply.github.com>
2021-07-28 14:42:29 +0530

    Update resource.tf

--- resource.tf
+++ resource.tf
@@ -8,7 +8,6 @@ terraform {
   }
 }
 provider "azurerm" {
-  features {}
   subscription_id = "6b806999-887d-4690-9e0b-dcb8b0504009"
   client_id       = "9e47a648-bc41-4c9e-b218-8f11d46b06ac"
   client_secret   = "777567a3-d96a-48a2-8e56-9cf9005790be"
