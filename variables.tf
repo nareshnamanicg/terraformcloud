708b2a8fa3a9d21bcd7c64ae5998d066e69b2768
nareshnamanicg <85550135+nareshnamanicg@users.noreply.github.com>
2021-07-29 11:45:17 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -10,9 +10,9 @@ terraform {
 provider "azurerm" {
 	features {}
 	subscription_id	= var.tf_var_arm_subscription_id
-	client_id  			= var.tf_var_arm_client_id
+	client_id       = var.tf_var_arm_client_id
 	client_secret 	= var.tf_var_arm_client_secret
-	tenant_id 			= var.tf_var_arm_tenant_id
+	tenant_id 	= var.tf_var_arm_tenant_id
 }
 
 # Variable declaration
