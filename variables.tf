c059372d180b6df6a1510f08ebf062b0eddc3a57
nareshnamanicg <85550135+nareshnamanicg@users.noreply.github.com>
2021-07-29 11:16:07 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,6 @@
 # Authentication
 provider "azurerm" {
 	version = "=1.38.0"
-	features {}
 	subscription_id = var.tf_var_arm_subscription_id
 	client_id       = var.tf_var_arm_client_id
 	client_secret 	= var.tf_var_arm_client_secret
